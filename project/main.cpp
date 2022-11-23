a256335ada21154b3350cafb7f9c4bd4de1a3fb3
xxx-2203 <s2119@e-mirim.hs.kr>
2022-11-24 01:46:42 +0900

    주석처리 및 화면 조정

--- project/main.cpp
+++ project/main.cpp
@@ -1,6 +1,7 @@
 #include <SFML/Graphics.hpp>
-#define WIDTH 650
-#define HEIGHT 650
+//크기는 650px가 적당하나 우선 기존 픽셀을 파악하기 위해 선정
+#define WIDTH 450
+#define HEIGHT 450
 
 using namespace sf;
 
@@ -9,11 +10,13 @@ int main(void)
 	RenderWindow app(VideoMode(WIDTH, HEIGHT), "foodGame");
 	app.setFramerateLimit(60);
 
+	//이미지 가져오기
 	Texture t;
 	t.loadFromFile("images/main.png");
 
 	Sprite Backgroud(t);
 
+	//앱 실행
 	while (app.isOpen())
 	{
 		Event e;
