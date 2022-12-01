f2b0ed1ae720bcc19a44245839469e4d269404fc
xxx-2203 <s2119@e-mirim.hs.kr>
2022-12-01 11:04:19 +0900

    bgm넣기

--- project/main.cpp
+++ project/main.cpp
@@ -1,4 +1,9 @@
 #include <SFML/Graphics.hpp>
+#include<time.h>
+#include <Windows.h> 
+#pragma comment(lib,"winmm.lib") 
+#include <mmsystem.h> 
+
 //크기는 650px가 적당하나 우선 기존 픽셀을 파악하기 위해 선정
 #define WIDTH 450
 #define HEIGHT 450
@@ -16,13 +21,21 @@ int main(void)
 
 	Sprite Backgroud(t);
 
+	//음악재생
+	PlaySound(TEXT("bgm.wav"), 0, SND_FILENAME | SND_ASYNC | SND_LOOP);
+
+	int cnt = 5;
+
 	//앱 실행
 	while (app.isOpen())
 	{
+
 		Event e;
 		while (app.pollEvent(e))
-		{
-			if (e.type == Event::Closed)
+		{	
+			Sleep(1000);
+			cnt -= 1;
+			if (cnt <0&&e.type == Event::Closed)
 				app.close();
 		}
 		app.clear();
--- project/test.cpp
+++ project/test.cpp
@@ -1,8 +1,22 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
+#include <time.h>
 
-int main(void)
-{
-	system("mode con cols=30 line=30 l title 제목명");
+int main() {
+    clock_t start, end;
+    double result;
+    int i, j;
+    int sum = 0;
 
+    start = clock(); //시간 측정 시작
+
+    for (i = 0; i < 100000; i++) {
+        for (j = 0; j < 10000; j++) {
+            sum += i * j;
+        }
+    }
+
+    end = clock(); //시간 측정 끝
+    result = (double)(end - start);
+    printf("%f", result);
+    return 0;
 }
